599d63d5506c66d0909e6571cb5fcfe48063ec71
FedericoPavesi <federico.pavesiwork@gmail.com>
2023-12-19 16:09:27 +0100

    First Commit

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 from __version__ import __version__
 
 setup(
-    name='autoprune',
+    name='AutoPrune',
     version=__version__,
 
-    url='https://github.com/FedericoPavesi/ohnn_repo',
+    url='https://github.com/fpavesi11/AutoPrune',
     author='Federico Pavesi',
     author_email='f.pavesi11@campus.unimib.it',
 
